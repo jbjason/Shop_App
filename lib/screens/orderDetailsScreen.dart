21c7bd291583cc9d3d8246a275241f4e0d6f30df
Jubayer <itisjubayer@gmail.com>
2021-04-24 11:42:22 +0600

    bar adding

--- lib/screens/orderDetailsScreen.dart
+++ lib/screens/orderDetailsScreen.dart
@@ -15,26 +15,18 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
   final _emailFocusNode = FocusNode();
   final _detailsFocusNode = FocusNode();
   var _isLoading = false, _isInit2 = false;
-  var _isInit = true, _isLoading2 = true;
   Map<String, String> _info = {
     'name': '',
     'email': '',
     'contact': '',
     'details': '',
   };
-
   @override
-  void didChangeDependencies() {
-    if (_isInit) {
-      Provider.of<Orders>(context, listen: false).fetchPoint().then((value) {
-        setState(() {});
-      });
-    }
-    setState(() {
-      _isLoading2 = false;
-      _isInit = false;
+  void initState() {
+    Provider.of<Orders>(context, listen: false).fetchPoint().then((value) {
+      setState(() {});
     });
-    super.didChangeDependencies();
+    super.initState();
   }
 
   @override
@@ -77,143 +69,153 @@ class _OrderDetailsScreenState extends State<OrderDetailsScreen> {
     });
   }
 
+  Widget pointBar(int p) {
+    RangeValues values = RangeValues(0, p.toDouble());
+    return RangeSlider(
+      values: values,
+      min: 0,
+      max: 100,
+      divisions: 50,
+      labels: RangeLabels(
+        values.start.round().toString(),
+        values.end.round().toString(),
+      ),
+      onChanged: (_) {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final fp = Provider.of<Orders>(context);
+    final fp = Provider.of<Orders>(context, listen: false).pointt;
     return Scaffold(
       appBar: AppBar(
         title: Text('Confirm ur details'),
       ),
-      body: _isLoading2
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : Padding(
-              padding: EdgeInsets.all(16),
-              child: Form(
-                  key: _form,
-                  child: ListView(
-                    children: [
-                      // name
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Name'),
-                        textInputAction: TextInputAction.next,
-                        onFieldSubmitted: (value) {
-                          FocusScope.of(context).requestFocus(_emailFocusNode);
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter a name !';
-                          }
-                          return null;
-                        },
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Form(
+            key: _form,
+            child: ListView(
+              children: [
+                // name
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Name'),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_emailFocusNode);
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a name !';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _info['name'] = value;
+                  },
+                ),
+                // email
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Email-address'),
+                  focusNode: _emailFocusNode,
+                  textInputAction: TextInputAction.done,
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_numberFocusNode);
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a email-address !';
+                    } else if (!value.contains('@') ||
+                        !value.contains('.com')) {
+                      return 'Invalid email-adrress';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _info['email'] = value;
+                  },
+                ),
+                //contact no
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Contact number'),
+                  focusNode: _numberFocusNode,
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(_detailsFocusNode);
+                  },
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter a contact number !';
+                    } else if (value.length < 11) {
+                      return 'Should be at least 11 characters';
+                    } else if (double.tryParse(value) == null) {
+                      return 'Please enter a valid number';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _info['contact'] = value;
+                  },
+                ),
+                //details
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Details address'),
+                  focusNode: _detailsFocusNode,
+                  maxLines: 3,
+                  keyboardType: TextInputType.multiline,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please provide full address !';
+                    } else if (value.length < 11) {
+                      return 'Should be at least 10 characters';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _info['details'] = value;
+                  },
+                ),
+                // pointBar
+                Container(
+                  margin: EdgeInsets.only(top: 20, bottom: 40),
+                  child: pointBar(fp),
+                ),
+                //voucher code
+                Container(
+                  height: 40,
+                  width: 80,
+                  //padding: EdgeInsets.all(20),
+                  child:
+                      Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    Text('Voucher code:   '),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: '     Y / N',
+                        ),
                         onSaved: (value) {
-                          _info['name'] = value;
-                        },
-                      ),
-                      // email
-                      TextFormField(
-                        decoration: InputDecoration(labelText: 'Email-address'),
-                        focusNode: _emailFocusNode,
-                        textInputAction: TextInputAction.done,
-                        onFieldSubmitted: (value) {
-                          FocusScope.of(context).requestFocus(_numberFocusNode);
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter a email-address !';
-                          } else if (!value.contains('@') ||
-                              !value.contains('.com')) {
-                            return 'Invalid email-adrress';
+                          if (value == 'Y' || value == 'y') {
+                            _isInit2 = true;
                           }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          _info['email'] = value;
                         },
                       ),
-                      //contact no
-                      TextFormField(
-                        decoration:
-                            InputDecoration(labelText: 'Contact number'),
-                        focusNode: _numberFocusNode,
-                        textInputAction: TextInputAction.next,
-                        keyboardType: TextInputType.number,
-                        onFieldSubmitted: (value) {
-                          FocusScope.of(context)
-                              .requestFocus(_detailsFocusNode);
-                        },
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please enter a contact number !';
-                          } else if (value.length < 11) {
-                            return 'Should be at least 11 characters';
-                          } else if (double.tryParse(value) == null) {
-                            return 'Please enter a valid number';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          _info['contact'] = value;
-                        },
-                      ),
-                      //details
-                      TextFormField(
-                        decoration:
-                            InputDecoration(labelText: 'Details address'),
-                        focusNode: _detailsFocusNode,
-                        maxLines: 3,
-                        keyboardType: TextInputType.multiline,
-                        validator: (value) {
-                          if (value.isEmpty) {
-                            return 'Please provide full address !';
-                          } else if (value.length < 11) {
-                            return 'Should be at least 10 characters';
-                          }
-                          return null;
-                        },
-                        onSaved: (value) {
-                          _info['details'] = value;
-                        },
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(top: 20, bottom: 40),
-                        child: Text(fp.pointt.toString()),
-                      ),
-                      Container(
-                        height: 40,
-                        width: 80,
-                        //padding: EdgeInsets.all(20),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              Text('Voucher code:   '),
-                              Expanded(
-                                child: TextFormField(
-                                  decoration: InputDecoration(
-                                    labelText: '     Y / N',
-                                  ),
-                                  onSaved: (value) {
-                                    if (value == 'Y' || value == 'y') {
-                                      _isInit2 = true;
-                                    }
-                                  },
-                                ),
-                              )
-                            ]),
-                      ),
-                      FlatButton(
-                        onPressed: () => submit(_isInit2, fp.pointt),
-                        child: _isLoading
-                            ? CircularProgressIndicator(
-                                backgroundColor: Colors.pink,
-                              )
-                            : Text('Commit to purchase'),
-                        textColor: Colors.green,
-                      ),
-                    ],
-                  )),
-            ),
+                    )
+                  ]),
+                ),
+
+                FlatButton(
+                  onPressed: () => submit(_isInit2, fp),
+                  child: _isLoading
+                      ? CircularProgressIndicator(
+                          backgroundColor: Colors.pink,
+                        )
+                      : Text('Commit to purchase'),
+                  textColor: Colors.green,
+                ),
+              ],
+            )),
+      ),
     );
   }
 }
