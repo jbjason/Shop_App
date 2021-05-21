e06851ea9ccb1d5173b434d345e3cff74cd84d6b
Jubayer <itisjubayer@gmail.com>
2021-05-22 01:35:57 +0600

    Update product_detail_screen.dart

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,8 +1,7 @@
-import 'package:Shop_App/widgets/comments.dart';
+import 'package:Shop_App/widgets/product_detail_screen_item.dart';
 import '../providers/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:smooth_star_rating/smooth_star_rating.dart';
 import '../providers/cart.dart';
 
 class ProductDetailScreen extends StatefulWidget {
@@ -45,191 +44,40 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     final productId = ModalRoute.of(context).settings.arguments as String;
     final loadedProduct = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(45),
-        child: AppBar(
-          backgroundColor: Color(0xFFC8E6C9),
-          title: Text(
-            loadedProduct.title,
-            style: TextStyle(color: Colors.black87),
-          ),
-          actions: [
-            Builder(
-              builder: (BuildContext context) {
-                return IconButton(
-                  icon: Icon(Icons.shopping_cart,
-                      color: Theme.of(context).accentColor),
-                  onPressed: () {
-                    cart.addItem(
-                      loadedProduct.id,
-                      loadedProduct.price,
-                      loadedProduct.imageUrl1,
-                      loadedProduct.title,
-                      1,
-                    );
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text('Added item to Cart!'),
-                      duration: Duration(seconds: 1),
-                    ));
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Container(
-              height: size.height,
-              width: size.width,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  //alignment: Alignment.centerLeft,
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/images/Bitmap.png'),
-                ),
-              ),
-            ),
-
-            // Upper Container Internal Work
-            Container(
-              margin: EdgeInsets.only(top: 20, left: 30, right: 30),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // image Container
-                  Container(
-                    height: 180,
-                    width: 180,
-                    child: FittedBox(
-                      fit: BoxFit.cover,
-                      child: Hero(
-                        tag: loadedProduct.id,
-                        child: Image.network(widget._isInit == 1
-                            ? loadedProduct.imageUrl1
-                            : widget.currentImageUrl),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-
-                  // color Button
-
-                  SizedBox(height: 20),
-
-                  // Title Text
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        ' ${loadedProduct.title}',
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      SmoothStarRating(
-                        size: 23,
-                        borderColor: Colors.amber,
-                        color: Colors.black,
-                        spacing: 2.0,
-                        //rating: ratingg,
-                        // onRated: (value) {
-                        //   ratingg = (ratingg + value) / 2;
-                        // },
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 5),
-
-                  // Price Text
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '\$ ${loadedProduct.price}',
-                        style: TextStyle(
-                            color: Colors.red,
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold),
-                      ),
-                      Text(' Subtitle '),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-
-                  // Description Text
-                  Text(
-                    loadedProduct.description,
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                    style: TextStyle(color: Colors.black, fontSize: 10),
-                  ),
-                  SizedBox(height: 70),
-
-                  // Item Count & Size DropDown box
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      DropdownButton(
-                          hint: Text(
-                            'Choose Size',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          value: _selectedLocation,
-                          items: _locations.map((e) {
-                            return DropdownMenuItem(
-                              child: Row(
-                                children: [
-                                  Icon(Icons.format_size),
-                                  SizedBox(
-                                    width: 15,
-                                  ),
-                                  Text(e),
-                                ],
-                              ),
-                              value: e,
-                            );
-                          }).toList(),
-                          onChanged: (newVal) {
-                            setState(() {
-                              _selectedLocation = newVal;
-                            });
-                          }),
-                    ],
-                  ),
-                  SizedBox(height: 15),
-
-                  // Order Now text Container
-                  Container(
-                    height: 37,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30),
-                      border: Border.all(),
-                    ),
-                    child: Text(
-                      'ORDER NOW',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  )
-                ],
-              ),
-            ),
-
-            Comments(),
-          ],
-        ),
-      ),
+      // appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(45),
+      //   child: AppBar(
+      //     backgroundColor: Color(0xFFC8E6C9),
+      //     title: Text(
+      //       loadedProduct.title,
+      //       style: TextStyle(color: Colors.black87),
+      //     ),
+      //     actions: [
+      //       Builder(
+      //         builder: (BuildContext context) {
+      //           return IconButton(
+      //             icon: Icon(Icons.shopping_cart,
+      //                 color: Theme.of(context).accentColor),
+      //             onPressed: () {
+      //               cart.addItem(
+      //                 loadedProduct.id,
+      //                 loadedProduct.price,
+      //                 loadedProduct.imageUrl1,
+      //                 loadedProduct.title,
+      //                 1,
+      //               );
+      //               Scaffold.of(context).showSnackBar(SnackBar(
+      //                 content: Text('Added item to Cart!'),
+      //                 duration: Duration(seconds: 1),
+      //               ));
+      //             },
+      //           );
+      //         },
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      body: ProductDetailScreenItem(),
     );
   }
 }
