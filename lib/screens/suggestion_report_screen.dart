fa2098e55ba30f8ec5c2d4730bb9d7c4d5d3a922
Jubayer <itisjubayer@gmail.com>
2021-06-03 12:31:22 +0600

    Update suggestion_report_screen.dart

--- lib/screens/suggestion_report_screen.dart
+++ lib/screens/suggestion_report_screen.dart
@@ -35,79 +35,87 @@ class _SuggestionReportScreenState extends State<SuggestionReportScreen> {
   @override
   Widget build(BuildContext context) {
     final userEmail = ModalRoute.of(context).settings.arguments as List<String>;
-    return Padding(
-      padding: EdgeInsets.all(16.0),
-      child: Form(
-        key: _form,
-        child: ListView(
-          children: [
-            TextFormField(
-              initialValue: userEmail[0],
-              readOnly: true,
-              decoration: InputDecoration(labelText: 'Email'),
-              textInputAction: TextInputAction.next,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_subjectFocusNode);
-              },
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please provide a value';
-                }
-                // return null means valid number
-                return null;
-              },
-              onSaved: (value) => email = value,
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Subject'),
-              textInputAction: TextInputAction.next,
-              focusNode: _subjectFocusNode,
-              onFieldSubmitted: (_) {
-                FocusScope.of(context).requestFocus(_descriptionFocusNode);
-              },
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please provide a value';
-                }
-                // return null means valid number
-                return null;
-              },
-              onSaved: (value) => subject = value,
-            ),
-            TextFormField(
-                decoration: InputDecoration(labelText: 'Whole in Details'),
-                textInputAction: TextInputAction.newline,
-                focusNode: _descriptionFocusNode,
-                maxLines: 5,
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFFC8E6C9),
+        title: Text('Your Cart',
+            style: TextStyle(fontSize: 20, color: Colors.black)),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Form(
+          key: _form,
+          child: ListView(
+            children: [
+              TextFormField(
+                initialValue: userEmail[0],
+                readOnly: true,
+                decoration: InputDecoration(labelText: 'Email'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_subjectFocusNode);
+                },
                 validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Please enter some text';
+                  if (value.isEmpty) {
+                    return 'Please provide a value';
                   }
                   // return null means valid number
                   return null;
                 },
-                onSaved: (value) => description = value),
-            SizedBox(height: 60),
-            Container(
-              height: 50,
-              //alignment: Alignment.centerRight,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
-                  onPrimary: Colors.white,
-                  shadowColor: Colors.greenAccent,
-                  elevation: 3,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(32.0)),
-                ),
-                onPressed: () => _saveForm(),
-                child: Text(
-                  'Submit',
-                  style: TextStyle(fontSize: 25),
+                onSaved: (value) => email = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Subject'),
+                textInputAction: TextInputAction.next,
+                focusNode: _subjectFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a value';
+                  }
+                  // return null means valid number
+                  return null;
+                },
+                onSaved: (value) => subject = value,
+              ),
+              TextFormField(
+                  decoration: InputDecoration(labelText: 'Whole in Details'),
+                  textInputAction: TextInputAction.newline,
+                  focusNode: _descriptionFocusNode,
+                  maxLines: 7,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    // return null means valid number
+                    return null;
+                  },
+                  onSaved: (value) => description = value),
+              SizedBox(height: 60),
+              Container(
+                height: 50,
+                //alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.green,
+                    onPrimary: Colors.white,
+                    shadowColor: Colors.greenAccent,
+                    elevation: 10,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32.0)),
+                  ),
+                  onPressed: () => _saveForm(),
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 25),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
