0cb22731af62c3f3a5242198e654d5a76d9daa25
Jubayer <itisjubayer@gmail.com>
2021-06-05 01:14:41 +0600

    Update return_productsList_screen.dart

--- lib/screens/return_productsList_screen.dart
+++ lib/screens/return_productsList_screen.dart
@@ -25,14 +25,10 @@ class _ReturnProductsListScreenState extends State<ReturnProductsListScreen> {
             Provider.of<Orders>(context, listen: false).fetchAndSetReturnList(),
         builder: (ctx, dataSnapshot) {
           if (dataSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
+            return Center(child: CircularProgressIndicator());
           } else {
             if (dataSnapshot.error != null) {
-              return Center(
-                child: Text('An error occured !'),
-              );
+              return Center(child: Text('An error occured !'));
             } else {
               return Consumer<Orders>(
                 builder: (ctx, product, _) => ListView.builder(
