16b971822a6eacdcd1a0b1c5f55e4d3c03c8340f
Jubayer <itisjubayer@gmail.com>
2021-06-04 23:15:56 +0600

    Update return_productsList_screen.dart

--- lib/screens/return_productsList_screen.dart
+++ lib/screens/return_productsList_screen.dart
@@ -1,4 +1,6 @@
+import 'package:Shop_App/providers/orders.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ReturnProductsListScreen extends StatefulWidget {
   static const routeName = '/return-products-list';
@@ -8,6 +10,17 @@ class ReturnProductsListScreen extends StatefulWidget {
 }
 
 class _ReturnProductsListScreenState extends State<ReturnProductsListScreen> {
+  var _isInit = false;
+  @override
+  void didChangeDependencies() {
+    if (_isInit == false) {
+      Provider.of<Orders>(context).fetchAndSetReturnList();
+    }
+    _isInit = true;
+    print('jb');
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
