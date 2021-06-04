7461bd686f9ac610df35516f314bcbb1075e766a
Jubayer <itisjubayer@gmail.com>
2021-06-04 23:08:08 +0600

    Update return_productsList_screen.dart

---
+++ lib/screens/return_productsList_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ReturnProductsListScreen extends StatefulWidget {
+  static const routeName = '/return-products-list';
+  @override
+  _ReturnProductsListScreenState createState() =>
+      _ReturnProductsListScreenState();
+}
+
+class _ReturnProductsListScreenState extends State<ReturnProductsListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
