9eb6e23a97002659d2959be75ded333122a6a482
Jubayer <itisjubayer@gmail.com>
2021-06-10 11:58:30 +0600

    testing .status

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -24,9 +24,24 @@ class _OrderItemState extends State<OrderItem> {
           children: [
             ListTile(
               title: Text('Total =  \$${widget.order.amount}'),
-              subtitle: Text(
-                DateFormat('dd/MM/yyyy    hh:mm').format(widget.order.dateTime),
+              subtitle: RichText(
+                text: TextSpan(children: [
+                  WidgetSpan(
+                    child: FlatButton.icon(
+                        onPressed: () {},
+                        color: Colors.amber[100],
+                        icon: Icon(Icons.arrow_circle_down),
+                        label: Text('Pending')),
+                  ),
+                  TextSpan(
+                      text: DateFormat('dd/MM/yyyy hh:mm')
+                          .format(widget.order.dateTime)),
+                ]),
               ),
+
+              // Text(
+              //   DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
+              // ),
               trailing: IconButton(
                   icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
                   onPressed: () {
