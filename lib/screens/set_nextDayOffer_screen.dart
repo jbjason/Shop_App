9628450b1594ded24120ae65c6aa6a7ee1ff4f09
Jubayer <itisjubayer@gmail.com>
2021-06-13 20:28:01 +0600

    next-day-offer page done

--- lib/screens/set_nextDayOffer_screen.dart
+++ lib/screens/set_nextDayOffer_screen.dart
@@ -8,12 +8,75 @@ class SetNextDayOfferScreen extends StatefulWidget {
 }
 
 class _SetNextDayOfferScreenState extends State<SetNextDayOfferScreen> {
+  final _imageFocusNode = FocusNode();
+  final _imageController = TextEditingController();
+
+  @override
+  void dispose() {
+    _imageController.removeListener(_updateImageUrl);
+    _imageController.dispose();
+    _imageFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if (!_imageFocusNode.hasFocus) {
+      if ((!_imageController.text.startsWith('http') &&
+              !_imageController.text.startsWith('https')) ||
+          (!_imageController.text.endsWith('.jpg') &&
+              !_imageController.text.endsWith('.png'))) {
+        return;
+      }
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFC8E6C9),
         title: Text('Manage Offers'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border.all(width: 2, color: Colors.green),
+              ),
+              child: _imageController.text.isEmpty
+                  ? Center(child: Text('Enter a URL'))
+                  : FittedBox(
+                      child: Image.network(
+                        _imageController.text,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(labelText: 'Image Url'),
+              focusNode: _imageFocusNode,
+              controller: _imageController,
+              keyboardType: TextInputType.url,
+              onFieldSubmitted: (_) {},
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a Url';
+                } else if ((!value.startsWith('http') &&
+                        !value.startsWith('https')) ||
+                    (!value.endsWith('.jpg') && !value.endsWith('.png'))) {
+                  return 'Please enter a valid Url';
+                }
+                return null;
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
