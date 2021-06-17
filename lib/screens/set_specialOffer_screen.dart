5ac1dd5fe4b7eae4679b14de7fe2427ae80ba2b6
Jubayer <itisjubayer@gmail.com>
2021-06-17 17:48:15 +0600

    set specialOffer page edit done

--- lib/screens/set_specialOffer_screen.dart
+++ lib/screens/set_specialOffer_screen.dart
@@ -1,15 +1,39 @@
+import 'package:Shop_App/providers/products.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SetSpecialOfferScreen extends StatefulWidget {
   static const routeName = '/set-special-offer';
-
   @override
   _SetSpecialOfferScreenState createState() => _SetSpecialOfferScreenState();
 }
 
 class _SetSpecialOfferScreenState extends State<SetSpecialOfferScreen> {
+  final _currentPriceFocusNode = FocusNode();
+  final _offerPriceFocusNode = FocusNode();
+  final _currentPriceController = TextEditingController();
+  final _offerPriceController = TextEditingController();
+  var _expanded = false;
+
+  void _save() {
+    final String old = _currentPriceController.text.trim();
+    final String offer = _offerPriceController.text.trim();
+  }
+
+  @override
+  void dispose() {
+    _currentPriceController.dispose();
+    _offerPriceController.dispose();
+    _currentPriceFocusNode.dispose();
+    _offerPriceFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final product =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -19,7 +43,137 @@ class _SetSpecialOfferScreenState extends State<SetSpecialOfferScreen> {
             style: TextStyle(fontSize: 20, color: Colors.black87),
           ),
         ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Card(
+                elevation: 7,
+                //margin: EdgeInsets.all(12),
+                child: ListTile(
+                  title: Text(
+                    'Instructions of removing offer',
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Text('  dfdjdf '),
+                  trailing: IconButton(
+                      icon: Icon(
+                        _expanded ? Icons.expand_less : Icons.expand_more,
+                        color: Colors.cyan,
+                        size: 35,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _expanded = !_expanded;
+                        });
+                      }),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 5),
+                height: _expanded == false ? 0 : 90,
+                width: double.infinity,
+                child: Card(
+                  //color: Colors.blueGrey[200],
+                  child: CondtionsForReturns(),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                children: [
+                  Expanded(
+                      child: Text(
+                    "Current Price    :",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: product.price.toString(),
+                      focusNode: _currentPriceFocusNode,
+                      controller: _currentPriceController,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.numberWithOptions(),
+                      onFieldSubmitted: (_) {
+                        FocusScope.of(context)
+                            .requestFocus(_offerPriceFocusNode);
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please provide a value';
+                        }
+                        return null;
+                      },
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 40),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                      child: Text(
+                    "Offer Price    :",
+                    style: TextStyle(
+                      fontSize: 17,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+                  Expanded(
+                      child: TextFormField(
+                    initialValue: product.extra,
+                    focusNode: _offerPriceFocusNode,
+                    controller: _offerPriceController,
+                    keyboardType: TextInputType.number,
+                    textInputAction: TextInputAction.done,
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please provide a value';
+                      }
+                      return null;
+                    },
+                  ))
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+}
+
+class CondtionsForReturns extends StatelessWidget {
+  const CondtionsForReturns({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(children: [
+        TextSpan(
+          text: '\n',
+        ),
+        WidgetSpan(
+            child: Icon(Icons.donut_small, size: 16, color: Colors.deepOrange)),
+        TextSpan(
+            text: '   1.  Remove the offer price.\n\n',
+            style: TextStyle(
+              color: Colors.black,
+            )),
+        WidgetSpan(
+            child: Icon(Icons.donut_small, size: 16, color: Colors.deepOrange)),
+        TextSpan(
+            text: '   2.  Leave the offer text either blank or type \'No\'\n',
+            style: TextStyle(
+              color: Colors.black,
+            )),
+      ]),
     );
   }
 }
