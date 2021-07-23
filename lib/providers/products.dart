91cd5572722ee02e20c529eb22508cfe3e060a16
Jubayer <itisjubayer@gmail.com>
2021-07-24 01:27:21 +0600

    colo & size er {} removing

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -214,7 +214,7 @@ class Products with ChangeNotifier {
             category: prodData['category'],
             extra: prodData['extra'],
             sizeList: (prodData['size'] as List<dynamic>)
-                .map((e) => {e['size1']}.toString())
+                .map((e) => e['size1'].toString())
                 .toList(),
             colorList: (prodData['color'] as List<dynamic>)
                 .map((e) => {e['color1']}.toString())
