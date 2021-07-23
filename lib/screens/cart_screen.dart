a3fd759de98572d81ce6e0579fc994722b4d3b2b
Jubayer <itisjubayer@gmail.com>
2021-07-23 20:26:42 +0600

    new cart screen & item  added

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,9 @@
-import '../providers/orders.dart';
-import '../providers/products.dart';
 import 'order_details_screen.dart';
 import '../providers/cart.dart' show Cart;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../widgets/cart_item.dart';
+import 'package:flutter/services.dart';
 
 class CartScreen extends StatefulWidget {
   static const routeName = '/cart';
@@ -22,71 +21,207 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Color(0xFFFDD148),
+      statusBarBrightness: Brightness.light,
+    ));
     final cart = Provider.of<Cart>(context);
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Color(0xFFC8E6C9),
-          title: Text('Your Cart',
-              style: TextStyle(fontSize: 20, color: Colors.black))),
-      body: Column(
-        children: [
-          Card(
-            elevation: 8,
-            margin: EdgeInsets.all(15),
-            child: Padding(
-              padding: EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Total ',
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  // it takes all free space as reserved
-                  Spacer(),
-                  Chip(
-                    label: Text(
-                      '\$ ${cart.totalAmount.toStringAsFixed(2)}',
-                      style: TextStyle(color: Colors.white),
+      body: Container(
+        height: size.height,
+        width: size.width,
+        child: Stack(children: [
+          ListView(children: [
+            Container(
+              height: size.height * .32,
+              width: size.width,
+              color: Color(0xFFFDD148),
+              child: Stack(children: [
+                Positioned(
+                  right: 100.0,
+                  child: Container(
+                    height: 400.0,
+                    width: 400.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(200.0),
+                      color: Color(0xFFFEE16D),
                     ),
-                    backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  //OrderButton(cart: cart),
-                  FlatButton(
-                    onPressed: (cart.totalAmount <= 0)
-                        ? null
-                        : () {
-                            Navigator.of(context)
-                                .pushNamed(OrderDetailsScreen.routeName);
-                          },
-                    child: Text('Order Now'),
-                    textColor: Theme.of(context).primaryColor,
+                ),
+                Positioned(
+                  bottom: 5.0,
+                  left: 150.0,
+                  child: Container(
+                      height: 300.0,
+                      width: 300.0,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(150.0),
+                          color: Color(0xFFFEE16D).withOpacity(0.5))),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 15.0),
+                  child: IconButton(
+                      alignment: Alignment.topLeft,
+                      icon: Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      }),
+                ),
+                Positioned(
+                    top: 75.0,
+                    left: 15.0,
+                    child: Text(
+                      'Shopping Cart',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold),
+                    )),
+              ]),
+            ),
+            Container(
+              height: size.height * .64,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Spacer(),
+                  Padding(
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Card(
+                      elevation: 16,
+                      child: Container(
+                          height: 55.0,
+                          width: double.infinity,
+                          color: Colors.white,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(width: 100),
+                              Text('Total: ',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.w500,
+                                      fontSize: 17)),
+                              Chip(
+                                label: Text(
+                                  '\$ ${cart.totalAmount.toStringAsFixed(2)}',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                backgroundColor: Theme.of(context).primaryColor,
+                              ),
+                              Spacer(),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: RaisedButton(
+                                  onPressed: (cart.totalAmount <= 0)
+                                      ? null
+                                      : () {
+                                          Navigator.of(context).pushNamed(
+                                              OrderDetailsScreen.routeName);
+                                        },
+                                  elevation: 0.5,
+                                  color: Colors.red,
+                                  child: Center(
+                                    child: Text(
+                                      'Settlement',
+                                    ),
+                                  ),
+                                  textColor: Colors.white,
+                                ),
+                              )
+                            ],
+                          )),
+                    ),
                   )
                 ],
               ),
             ),
+          ]),
+          Positioned(
+            bottom: size.height * .1,
+            child: Container(
+                height: size.height * .64,
+                width: size.width,
+                child: ListView.builder(
+                  itemBuilder: (ctx, i) => CartItem(
+                    // if we wanna fetch a value from selected Map item then we
+                    // need to add .values.toList()
+                    cart.items.values.toList()[i].id,
+                    // special keyword to get the selected id's random key ,which will be needed to update,delete the cart items
+                    cart.items.keys.toList()[i],
+                    cart.items.values.toList()[i].title,
+                    cart.items.values.toList()[i].price,
+                    cart.items.values.toList()[i].quantity,
+                    cart.items.values.toList()[i].imageUrl,
+                    updateCart,
+                    cart.items.values.toList()[i].color,
+                    cart.items.values.toList()[i].size,
+                  ),
+                  itemCount: cart.itemCount,
+                )),
           ),
-          SizedBox(height: 10),
-          Expanded(
-              child: ListView.builder(
-            itemBuilder: (ctx, i) => CartItem(
-              // if we wanna fetch a value from selected Map item then we
-              // need to add .values.toList()
-              cart.items.values.toList()[i].id,
-              // special keyword to get the selected id's random key ,which will be needed to update,delete the cart items
-              cart.items.keys.toList()[i],
-              cart.items.values.toList()[i].title,
-              cart.items.values.toList()[i].price,
-              cart.items.values.toList()[i].quantity,
-              cart.items.values.toList()[i].imageUrl,
-              updateCart,
-              cart.items.values.toList()[i].color,
-              cart.items.values.toList()[i].size,
-            ),
-            itemCount: cart.itemCount,
-          )),
-        ],
+        ]),
       ),
     );
   }
 }
+
+// body: Column(
+      //   children: [
+      //     Card(
+      //       elevation: 8,
+      //       margin: EdgeInsets.all(15),
+      //       child: Padding(
+      //         padding: EdgeInsets.all(8),
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //           children: [
+      //             Text(
+      //               'Total ',
+      //               style: TextStyle(fontSize: 30),
+      //             ),
+      //             // it takes all free space as reserved
+      //             Spacer(),
+      //             Chip(
+      //               label: Text(
+      //                 '\$ ${cart.totalAmount.toStringAsFixed(2)}',
+      //                 style: TextStyle(color: Colors.white),
+      //               ),
+      //               backgroundColor: Theme.of(context).primaryColor,
+      //             ),
+      //             //OrderButton(cart: cart),
+      //             FlatButton(
+      //               onPressed: (cart.totalAmount <= 0)
+      //                   ? null
+      //                   : () {
+      //                       Navigator.of(context)
+      //                           .pushNamed(OrderDetailsScreen.routeName);
+      //                     },
+      //               child: Text('Order Now'),
+      //               textColor: Theme.of(context).primaryColor,
+      //             )
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //     SizedBox(height: 10),
+      // Expanded(
+      //     child: ListView.builder(
+      //   itemBuilder: (ctx, i) => CartItem(
+      //     // if we wanna fetch a value from selected Map item then we
+      //     // need to add .values.toList()
+      //     cart.items.values.toList()[i].id,
+      //     // special keyword to get the selected id's random key ,which will be needed to update,delete the cart items
+      //     cart.items.keys.toList()[i],
+      //     cart.items.values.toList()[i].title,
+      //     cart.items.values.toList()[i].price,
+      //     cart.items.values.toList()[i].quantity,
+      //     cart.items.values.toList()[i].imageUrl,
+      //     updateCart,
+      //     cart.items.values.toList()[i].color,
+      //     cart.items.values.toList()[i].size,
+      //   ),
+      //   itemCount: cart.itemCount,
+      // )),
+      //   ],
+      // ),
--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -58,77 +58,219 @@ class _CartItemState extends State<CartItem> {
       onDismissed: (direction) {
         Provider.of<Cart>(context, listen: false).removeItem(widget.productId);
       },
-      child: Card(
-        elevation: 3,
-        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListTile(
-            leading: Padding(
-              padding: EdgeInsets.all(5),
-              child: FittedBox(
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      widget.imageUrl,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            title: Text(widget.title),
-            subtitle: Text(
-                'Price: \$${(widget.price)}   ${widget.size} ${widget.color}'),
-            trailing: Container(
-              width: 80,
-              child: Row(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Material(
+          borderRadius: BorderRadius.circular(10.0),
+          elevation: 7.0,
+          child: Container(
+            padding: EdgeInsets.only(left: 15.0, right: 10.0),
+            width: MediaQuery.of(context).size.width - 20.0,
+            height: 150.0,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
+            child: Row(children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    height: 24,
-                    width: 24,
-                    child: OutlineButton(
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Icon(Icons.remove),
-                      onPressed: () {
-                        if (count <= 1) {
-                        } else {
-                          setState(() {
-                            count--;
-                            widget.updateFunction(widget.productId, count);
-                          });
-                        }
-                      },
-                    ),
-                  ),
-                  SizedBox(width: 3),
-                  Text(count.toString().padLeft(2, '0')),
-                  SizedBox(width: 3),
-                  SizedBox(
-                    height: 23,
-                    width: 25,
-                    child: OutlineButton(
-                      padding: EdgeInsets.zero,
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Icon(Icons.add),
-                      onPressed: () {
-                        setState(() {
-                          count++;
-                          widget.updateFunction(widget.productId, count);
-                        });
-                      },
-                    ),
-                  ),
-                  //SizedBox(width: 10),
+                  Container(
+                      height: 25.0,
+                      width: 25.0,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.withOpacity(0.4),
+                        borderRadius: BorderRadius.circular(12.5),
+                      ),
+                      child: Center(
+                          child: Container(
+                        height: 12.0,
+                        width: 12.0,
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(6.0)),
+                      )))
                 ],
               ),
-            ),
+              SizedBox(width: 10.0),
+              Container(
+                height: 150.0,
+                width: 125.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(widget.imageUrl),
+                      fit: BoxFit.contain),
+                ),
+              ),
+              SizedBox(width: 4.0),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${widget.title}',
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15.0),
+                  ),
+                  SizedBox(height: 7.0),
+                  Text(
+                    'Color:  ${widget.color}',
+                    style: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.0,
+                        color: Colors.grey),
+                  ),
+                  Text(
+                    'Size: ${widget.size}',
+                    style: TextStyle(
+                        fontFamily: 'Quicksand',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14.0,
+                        color: Colors.grey),
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(children: [
+                    Text(
+                      '\$${widget.price}',
+                      style: TextStyle(
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Color(0xFFFDD34A)),
+                    ),
+                    SizedBox(width: 20),
+                    Container(
+                      width: 80,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 22,
+                            width: 22,
+                            child: OutlineButton(
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Icon(Icons.remove, size: 12),
+                              onPressed: () {
+                                if (count <= 1) {
+                                } else {
+                                  setState(() {
+                                    count--;
+                                    widget.updateFunction(
+                                        widget.productId, count);
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                          SizedBox(width: 3),
+                          Text(count.toString().padLeft(2, '0')),
+                          SizedBox(width: 3),
+                          SizedBox(
+                            height: 25,
+                            width: 27,
+                            child: OutlineButton(
+                              color: Colors.lightBlue,
+                              padding: EdgeInsets.zero,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.lightBlue,
+                                size: 24,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  count++;
+                                  widget.updateFunction(
+                                      widget.productId, count);
+                                });
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ]),
+                ],
+              )
+            ]),
           ),
         ),
       ),
+      // child: Card(
+      //   elevation: 3,
+      //   margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+      //   child: Padding(
+      //     padding: EdgeInsets.all(8),
+      //     child: ListTile(
+      //       leading: Padding(
+      //         padding: EdgeInsets.all(5),
+      //         child: FittedBox(
+      //           child: FittedBox(
+      //             fit: BoxFit.cover,
+      //             child: CircleAvatar(
+      //               backgroundImage: NetworkImage(
+      //                 widget.imageUrl,
+      //               ),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //       title: Text(widget.title),
+      //       subtitle: Text(
+      //           'Price: \$${(widget.price)}   ${widget.size} ${widget.color}'),
+      //       trailing: Container(
+      //         width: 80,
+      //         child: Row(
+      //           children: [
+      //             SizedBox(
+      //               height: 24,
+      //               width: 24,
+      //               child: OutlineButton(
+      //                 padding: EdgeInsets.zero,
+      //                 shape: RoundedRectangleBorder(
+      //                     borderRadius: BorderRadius.circular(10)),
+      //                 child: Icon(Icons.remove),
+      //                 onPressed: () {
+      //                   if (count <= 1) {
+      //                   } else {
+      //                     setState(() {
+      //                       count--;
+      //                       widget.updateFunction(widget.productId, count);
+      //                     });
+      //                   }
+      //                 },
+      //               ),
+      //             ),
+      //             SizedBox(width: 3),
+      //             Text(count.toString().padLeft(2, '0')),
+      //             SizedBox(width: 3),
+      //             SizedBox(
+      //               height: 23,
+      //               width: 25,
+      //               child: OutlineButton(
+      //                 padding: EdgeInsets.zero,
+      //                 shape: RoundedRectangleBorder(
+      //                     borderRadius: BorderRadius.circular(10)),
+      //                 child: Icon(Icons.add),
+      //                 onPressed: () {
+      //                   setState(() {
+      //                     count++;
+      //                     widget.updateFunction(widget.productId, count);
+      //                   });
+      //                 },
+      //               ),
+      //             ),
+      //             //SizedBox(width: 10),
+      //           ],
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
