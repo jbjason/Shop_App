8a3d57fce6436bdf91f13c78e5ca3e617645bbcd
Jubayer <itisjubayer@gmail.com>
2021-07-25 03:24:45 +0600

    productsGrid page baad,all to overViewScreen

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,5 +1,3 @@
-import 'package:Shop_App/providers/product.dart';
-
 import '../providers/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -126,17 +124,17 @@ class _ProductsGridState extends State<ProductsGrid> {
                       hintText: 'Search',
                       hintStyle: TextStyle(
                           color: Colors.grey, fontFamily: 'Quicksand')),
-                  onTap: () async {
-                    final result = await showSearch(
-                        context: context,
-                        delegate: DataSearch(productsData.searcHints));
-                    if (result != null) {
-                      setState(() {
-                        productsData.setSerachForProduct(result);
-                        _isSearch = true;
-                      });
-                    }
-                  },
+                  // onTap: () async {
+                  //   final result = await showSearch(
+                  //       context: context,
+                  //       delegate: DataSearch(productsData.searcHints));
+                  //   if (result != null) {
+                  //     setState(() {
+                  //       productsData.setSerachForProduct(result);
+                  //       _isSearch = true;
+                  //     });
+                  //   }
+                  // },
                 ),
               ),
             ),
@@ -205,7 +203,7 @@ class _ProductsGridState extends State<ProductsGrid> {
                   fontSize: 23.0,
                   fontWeight: FontWeight.bold),
             ),
-            PopularProducts(productsData.items),
+            //PopularProducts(productsData.items),
             Text(
               'Reccomended For You',
               style: TextStyle(
@@ -228,267 +226,6 @@ class _ProductsGridState extends State<ProductsGrid> {
         ),
       ),
     );
-  }
-}
-
-class PopularProducts extends StatelessWidget {
-  // final String name, cat, id;
-  final List<Product> _relatedList;
-  PopularProducts(this._relatedList);
-  Text _buildRatingStars(int rating) {
-    String stars = '';
-    for (int i = 0; i < rating; i++) {
-      stars += '⭐';
-    }
-    return Text(stars);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 330,
-      padding: EdgeInsets.only(bottom: 15, left: 5, right: 5),
-      child: ListView(
-        physics: NeverScrollableScrollPhysics(),
-        children: <Widget>[
-          Container(
-            height: 304.0,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (BuildContext context, int index) {
-                return InkWell(
-                  onTap: () {
-                    // Navigator.of(context).pushNamed(
-                    //   ProductDetailScreen.routeName,
-                    //   arguments: _relatedList[index].id,
-                    // );
-                  },
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Card(
-                      elevation: 3,
-                      child: Container(
-                        //color: Color(0xFFFDD148).withOpacity(0.7),
-                        margin: EdgeInsets.all(10.0),
-                        width: 210.0,
-                        child: Stack(
-                          alignment: Alignment.topCenter,
-                          children: <Widget>[
-                            Positioned(
-                              bottom: 5.0,
-                              child: Container(
-                                height: 100.0,
-                                width: 200.0,
-                                decoration: BoxDecoration(
-                                  color: Color(0xFFFDD148).withOpacity(0.3),
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Padding(
-                                  padding: EdgeInsets.all(10.0),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: <Widget>[
-                                      // price & offer Price Row
-                                      Row(
-                                        children: [
-                                          Text(
-                                            '\$ ${_relatedList[index].price}',
-                                            style: TextStyle(
-                                                fontSize: 15.0,
-                                                fontWeight: FontWeight.w600,
-                                                // decoration: _relatedList[index]
-                                                //             .extra !=
-                                                //         "no"
-                                                //     ? TextDecoration.lineThrough
-                                                //     : TextDecoration.none,
-                                                decorationThickness: 2,
-                                                decorationColor: Colors.red),
-                                          ),
-                                          Spacer(),
-                                          //_relatedList[index].extra != "no" ?
-                                          Text(
-                                            '\$ 45',
-                                            style: TextStyle(
-                                              fontSize: 15.0,
-                                              fontWeight: FontWeight.w800,
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      // icon & Reviews
-                                      Row(
-                                        children: <Widget>[
-                                          Icon(
-                                            Icons.location_on_sharp,
-                                            size: 10.0,
-                                            color: Colors.black,
-                                          ),
-                                          SizedBox(width: 5.0),
-                                          Text(
-                                            '${11} activities',
-                                            style: TextStyle(
-                                              fontSize: 11,
-                                              color: Colors.black,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                      // ratings
-                                      _buildRatingStars(5),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(20.0),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.black26,
-                                    offset: Offset(0.0, 2.0),
-                                    blurRadius: 6.0,
-                                  ),
-                                ],
-                              ),
-                              child: Stack(
-                                children: <Widget>[
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(20.0),
-                                    child: Image.network(
-                                      _relatedList[index].imageUrl1,
-                                      height: 180.0,
-                                      width: 180.0,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                  Positioned(
-                                    left: 5.0,
-                                    bottom: 10.0,
-                                    child: Container(
-                                      color: Colors.black.withOpacity(0.5),
-                                      child: Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Text(
-                                            _relatedList[index].title,
-                                            overflow: TextOverflow.ellipsis,
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 24.0,
-                                              fontWeight: FontWeight.w600,
-                                              letterSpacing: 1.2,
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-              itemCount: _relatedList.length,
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class DataSearch extends SearchDelegate<String> {
-  final List<String> _hints;
-  DataSearch(this._hints);
-  final recentCities = [
-    "Red T-shirt",
-    "Munich",
-    "Paris",
-  ];
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    try {
-      return [
-        IconButton(
-            icon: Icon(Icons.clear),
-            onPressed: () {
-              query = "";
-            })
-      ];
-    } catch (error) {
-      throw UnimplementedError();
-    }
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    try {
-      return IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            close(context, null);
-          });
-    } catch (error) {
-      throw UnimplementedError();
-    }
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    //return
-    return Center(
-      child: Text(
-        query,
-        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-      ),
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    try {
-      final suggestionList = query.isEmpty
-          ? recentCities
-          : _hints.where((p) {
-              final cityLower = p.toLowerCase();
-              final queryLower = query.toLowerCase();
-              return cityLower.startsWith(queryLower);
-            }).toList();
-      return ListView.builder(
-        itemBuilder: (context, index) => ListTile(
-          onTap: () {
-            query = suggestionList[index];
-            close(context, query);
-          },
-          leading: Icon(Icons.location_city),
-          title: RichText(
-            text: TextSpan(
-                text: suggestionList[index].substring(0, query.length),
-                style:
-                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
-                children: [
-                  TextSpan(
-                    text: suggestionList[index].substring(query.length),
-                    style: TextStyle(color: Colors.grey),
-                  ),
-                ]),
-          ),
-        ),
-        itemCount: suggestionList.length,
-      );
-    } catch (error) {
-      throw UnimplementedError();
-    }
   }
 }
 
