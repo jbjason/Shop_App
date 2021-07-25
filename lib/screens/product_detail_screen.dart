a7ecbf632ccc5c33622aab3b97d30c917d21dd80
Jubayer <itisjubayer@gmail.com>
2021-07-25 15:32:58 +0600

    Update product_detail_screen.dart

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -2,48 +2,20 @@ import 'package:Shop_App/widgets/product_detail_screen_item.dart';
 import '../providers/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+    ));
     final productId = ModalRoute.of(context).settings.arguments as String;
     final loadedProduct = Provider.of<Products>(context).findById(productId);
     return Scaffold(
-      // appBar: PreferredSize(
-      //   preferredSize: Size.fromHeight(45),
-      //   child: AppBar(
-      //     backgroundColor: Color(0xFFC8E6C9),
-      //     title: Text(
-      //       loadedProduct.title,
-      //       style: TextStyle(color: Colors.black87),
-      //     ),
-      //     actions: [
-      //       Builder(
-      //         builder: (BuildContext context) {
-      //           return IconButton(
-      //             icon: Icon(Icons.shopping_cart,
-      //                 color: Theme.of(context).accentColor),
-      //             onPressed: () {
-      //               cart.addItem(
-      //                 loadedProduct.id,
-      //                 loadedProduct.price,
-      //                 loadedProduct.imageUrl1,
-      //                 loadedProduct.title,
-      //                 1,
-      //               );
-      //               Scaffold.of(context).showSnackBar(SnackBar(
-      //                 content: Text('Added item to Cart!'),
-      //                 duration: Duration(seconds: 1),
-      //               ));
-      //             },
-      //           );
-      //         },
-      //       ),
-      //     ],
-      //   ),
-      // ),
       body: ProductDetailScreenItem(
         loadedProduct.id,
         loadedProduct.title,
