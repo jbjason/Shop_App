d7fe96d1e7a29773290d01b2f433d07708cf3897
Jubayer <itisjubayer@gmail.com>
2021-07-25 16:08:10 +0600

    return null if valid ,added to authScreen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -231,6 +231,7 @@ class _AuthCardState extends State<AuthCard>
                     if (value.isEmpty || !value.contains('@')) {
                       return 'Invalid email!';
                     }
+                    return null;
                   },
                   onSaved: (value) {
                     _authData['email'] = value;
@@ -247,6 +248,7 @@ class _AuthCardState extends State<AuthCard>
                     if (value.isEmpty || value.length < 5) {
                       return 'Password is too short!';
                     }
+                    return null;
                   },
                   onSaved: (value) {
                     _authData['password'] = value;
@@ -271,6 +273,7 @@ class _AuthCardState extends State<AuthCard>
                               if (value != _passwordController.text) {
                                 return 'Passwords do not match!';
                               }
+                              return null;
                             }
                           : null,
                     ),
