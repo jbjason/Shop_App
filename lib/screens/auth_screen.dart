d19ddfe0da598ec5971855052ab3b2ade9ef1d8e
Jubayer <itisjubayer@gmail.com>
2021-07-26 11:57:49 +0600

    AuthScreen design done

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,8 +1,8 @@
-import 'dart:math';
 import '../models/http_exception.dart';
 import '../providers/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flare_flutter/flare_actor.dart';
 
 enum AuthMode { Signup, Login }
 
@@ -14,70 +14,41 @@ class AuthScreen extends StatelessWidget {
     // final transformConfig = Matrix4.rotationZ(-8 * pi / 180);
     // transformConfig.translate(-10.0);
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-                  Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: [0, 1],
-              ),
-            ),
-          ),
-          SingleChildScrollView(
-            child: Container(
-              height: deviceSize.height,
-              width: deviceSize.width,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Flexible(
-                    child: Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      padding:
-                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 94.0),
-                      transform: Matrix4.rotationZ(-8 * pi / 180)
-                        ..translate(-10.0),
-                      // ..translate(-10.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(20),
-                        color: Colors.deepOrange.shade900,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 8,
-                            color: Colors.black26,
-                            offset: Offset(0, 2),
-                          )
-                        ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: deviceSize.height,
+          width: deviceSize.width,
+          child: Stack(children: [
+            LoginDesign1(deviceSize: deviceSize),
+            LoginDesign2(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: deviceSize.height * .2,
+                  width: 250,
+                  margin: EdgeInsets.only(bottom: 20.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.deepOrange.shade900,
+                    image: DecorationImage(
+                      image: AssetImage(
+                        'assets/images/homeLogo2.jpeg',
                       ),
-                      child: Text(
-                        'MyShop',
-                        style: TextStyle(
-                          color:
-                              Theme.of(context).accentTextTheme.headline6.color,
-                          fontSize: 50,
-                          fontFamily: 'Anton',
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
+                      fit: BoxFit.contain,
                     ),
                   ),
-                  Flexible(
-                    flex: deviceSize.width > 600 ? 2 : 1,
-                    child: AuthCard(),
-                  ),
-                ],
-              ),
+                ),
+                Container(
+                  height: deviceSize.height * .65 + 15.0,
+                  color: Colors.transparent,
+                  child: AuthCard(),
+                )
+              ],
             ),
-          ),
-        ],
+          ]),
+        ),
       ),
     );
   }
@@ -92,38 +63,55 @@ class AuthCard extends StatefulWidget {
   _AuthCardState createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard>
-    with SingleTickerProviderStateMixin {
+class _AuthCardState extends State<AuthCard> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.Login;
   Map<String, String> _authData = {
     'email': '',
     'password': '',
   };
+
   var _isLoading = false;
   final _passwordController = TextEditingController();
-  AnimationController _controller;
-  Animation<double> _opacityAnimation;
-  Animation<Size> _heightAnimation;
+  final _userNameController = TextEditingController();
+  FocusNode _passwordNode = FocusNode();
+  FocusNode _userNameNode = FocusNode();
+  String _anitmationType = 'idle';
 
   @override
   void initState() {
-    // this makes sure what to control
-    _controller =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    // Tween means between . actually kinda like if condition
-    _heightAnimation = Tween<Size>(
-            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
-    //_heightAnimation.addListener(() => setState(() {}));
-    _opacityAnimation = Tween(begin: 0.0, end: 1.0)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    _passwordNode.addListener(() {
+      if (_passwordNode.hasFocus) {
+        setState(() {
+          _anitmationType = 'hands_up';
+        });
+      } else {
+        setState(() {
+          _anitmationType = 'hands_down';
+        });
+      }
+    });
+    _userNameNode.addListener(() {
+      if (_userNameNode.hasFocus) {
+        setState(() {
+          _anitmationType = 'test';
+        });
+      } else {
+        setState(() {
+          _anitmationType = 'idle';
+        });
+      }
+    });
     super.initState();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _passwordNode.dispose();
+    _userNameNode.dispose();
+    _passwordController.dispose();
+    _userNameController.dispose();
+
     super.dispose();
   }
 
@@ -146,7 +134,6 @@ class _AuthCardState extends State<AuthCard>
 
   Future<void> _submit() async {
     if (!_formKey.currentState.validate()) {
-      // Invalid!
       return;
     }
     _formKey.currentState.save();
@@ -162,8 +149,15 @@ class _AuthCardState extends State<AuthCard>
         // Sign user up
         await Provider.of<Auth>(context, listen: false)
             .signup(_authData['email'], _authData['password']);
+
+        setState(() {
+          _anitmationType = 'success';
+        });
       }
     } on HttpException catch (error) {
+      setState(() {
+        _anitmationType = 'fail';
+      });
       // Manual error/Exception Handling
       var errorMsg = 'Authentication failed';
       if (error.toString().contains('EMAIL_EXISTS')) {
@@ -179,8 +173,12 @@ class _AuthCardState extends State<AuthCard>
       }
       _showErrorDialog(errorMsg);
     } catch (error) {
+      setState(() {
+        _anitmationType = 'fail';
+      });
       _showErrorDialog('Could not authinticate you. Please try again later');
     }
+    await Future.delayed(Duration(milliseconds: 400));
     setState(() {
       _isLoading = false;
     });
@@ -191,82 +189,104 @@ class _AuthCardState extends State<AuthCard>
       setState(() {
         _authMode = AuthMode.Signup;
       });
-      // start animating by .forward()
-      _controller.forward();
     } else {
       setState(() {
         _authMode = AuthMode.Login;
       });
-      _controller.reverse();
     }
   }
 
   @override
   Widget build(BuildContext context) {
     final deviceSize = MediaQuery.of(context).size;
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-      elevation: 8.0,
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeIn,
-        height: _authMode == AuthMode.Signup ? 320 : 260,
-        //height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _authMode == AuthMode.Signup ? 320 : 260),
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'E-Mail', prefixIcon: Icon(Icons.people)),
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    if (value.isEmpty || !value.contains('@')) {
-                      return 'Invalid email!';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _authData['email'] = value;
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                      labelText: 'Password', prefixIcon: Icon(Icons.lock)),
-
-                  // password text star hishebe show korbe
-                  obscureText: true,
-                  controller: _passwordController,
-                  validator: (value) {
-                    if (value.isEmpty || value.length < 5) {
-                      return 'Password is too short!';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _authData['password'] = value;
-                  },
-                ),
-                AnimatedContainer(
-                  duration: Duration(milliseconds: 300),
-                  curve: Curves.easeIn,
-                  constraints: BoxConstraints(
-                      minHeight: _authMode == AuthMode.Signup ? 60 : 0,
-                      maxHeight: _authMode == AuthMode.Signup ? 120 : 0),
-                  child: FadeTransition(
-                    opacity: _opacityAnimation,
-                    child: TextFormField(
+    return Container(
+      width: deviceSize.width,
+      padding: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: 300,
+              child: FlareActor(
+                'assets/images/Teddy.flr',
+                alignment: Alignment.bottomCenter,
+                fit: BoxFit.contain,
+                animation: this._anitmationType,
+                callback: (_) {
+                  setState(() {
+                    this._anitmationType = 'idle';
+                  });
+                },
+              ),
+            ),
+            Container(
+              padding:
+                  EdgeInsets.only(left: 15, right: 15, top: 10, bottom: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: Colors.white,
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(children: [
+                  TextFormField(
+                    focusNode: _userNameNode,
+                    decoration: InputDecoration(
+                      hintText: ' E-Mail',
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontFamily: 'Quicksand'),
+                      prefixIcon: Icon(Icons.mark_email_read,
+                          size: 30.0,
+                          color:
+                              Color.fromRGBO(38, 50, 56, 1).withOpacity(0.8)),
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Invalid email!';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _authData['email'] = value;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: ' Password',
+                      hintStyle: TextStyle(
+                          color: Colors.grey, fontFamily: 'Quicksand'),
+                      prefixIcon:
+                          Icon(Icons.lock, size: 30.0, color: Colors.green),
+                      contentPadding: EdgeInsets.all(20),
+                    ),
+                    // password text star hishebe show korbe
+                    obscureText: true,
+                    controller: _passwordController,
+                    focusNode: _passwordNode,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 5) {
+                        return 'Password is too short!';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _authData['password'] = value;
+                    },
+                  ),
+                  if (_authMode == AuthMode.Signup)
+                    TextFormField(
                       enabled: _authMode == AuthMode.Signup,
                       decoration: InputDecoration(
-                          labelText: 'Confirm Password',
-                          prefixIcon: Icon(Icons.lock)),
+                        hintText: ' Confirm Password',
+                        hintStyle: TextStyle(
+                            color: Colors.grey, fontFamily: 'Quicksand'),
+                        prefixIcon:
+                            Icon(Icons.lock, size: 30.0, color: Colors.green),
+                        contentPadding: EdgeInsets.all(20),
+                      ),
                       obscureText: true,
                       validator: _authMode == AuthMode.Signup
                           ? (value) {
@@ -277,39 +297,115 @@ class _AuthCardState extends State<AuthCard>
                             }
                           : null,
                     ),
+                  SizedBox(
+                    height: 20,
                   ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                if (_isLoading)
-                  CircularProgressIndicator()
-                else
-                  RaisedButton(
-                    child:
-                        Text(_authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
-                    onPressed: _submit,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
+                  if (_isLoading)
+                    CircularProgressIndicator()
+                  else
+                    RaisedButton(
+                      child: Text(
+                          _authMode == AuthMode.Login ? 'LOGIN' : 'SIGN UP'),
+                      onPressed: _submit,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
+                      color: Theme.of(context).primaryColor,
+                      textColor:
+                          Theme.of(context).primaryTextTheme.button.color,
                     ),
+                  FlatButton(
+                    child: Text(
+                        '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
+                    onPressed: _switchAuthMode,
                     padding:
-                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 8.0),
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).primaryTextTheme.button.color,
+                        EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    textColor: Theme.of(context).primaryColor,
                   ),
-                FlatButton(
-                  child: Text(
-                      '${_authMode == AuthMode.Login ? 'SIGNUP' : 'LOGIN'} INSTEAD'),
-                  onPressed: _switchAuthMode,
-                  padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 4),
-                  materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                  textColor: Theme.of(context).primaryColor,
-                ),
-              ],
+                ]),
+              ),
             ),
-          ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class LoginDesign1 extends StatelessWidget {
+  const LoginDesign1({
+    Key key,
+    @required this.deviceSize,
+  }) : super(key: key);
+
+  final Size deviceSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 0,
+      child: Container(
+        height: 550,
+        width: deviceSize.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/welcomeShop.jpg')),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginDesign2 extends StatelessWidget {
+  const LoginDesign2({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 400,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.fill),
+      ),
+      child: Stack(children: [
+        Positioned(
+          left: 30,
+          width: 80,
+          height: 200,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/light1.png'))),
+          ),
+        ),
+        Positioned(
+          left: 140,
+          width: 80,
+          height: 150,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/light2.png'))),
+          ),
+        ),
+        Positioned(
+          right: 40,
+          top: 40,
+          width: 80,
+          height: 150,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/clock.png'))),
+          ),
+        ),
+      ]),
     );
   }
 }
